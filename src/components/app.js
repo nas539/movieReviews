8378d2c0136181487bb0e28e70ea47c1e9ccd8ce
Nicholas Sharp <naas8905@gmail.com>
2020-08-08 12:53:34 -0700

    Feature: adding the nav to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navigation from "./navigation/navigation"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        
+        <Navigation />
       </div>
     );
   }
