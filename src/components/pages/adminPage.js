d033564bcfe5e9c9a496e3df29922f50aa3f2899
Nicholas Sharp <naas8905@gmail.com>
2020-08-08 16:21:57 -0700

    Feature: setting up admin page

--- src/components/pages/adminPage.js
+++ src/components/pages/adminPage.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+import CreateMovie from '../adminComponents/createMovie';
+import EditMovie from '../adminComponents/editMovie';
+
+export default function adminPage() {
+    return (
+        <div className="admin-body-wrapper">
+            <CreateMovie />
+            <EditMovie />
+        </div>
+    )
+}
 
