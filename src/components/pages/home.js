4802d55d9a1b67b078300b64117813a1378aa626
Nicholas Sharp <naas8905@gmail.com>
2020-08-08 12:01:27 -0700

    Feature: homepage

---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function home() {
+    return (
+        <div className="home-page-body-wrapper">
+            <div className="home-page-info-wrapper">
+                <p className="home-page-info">
+                    Welcome to Scalar's Movie Review Assessment
+                </p>
+            </div>
+        </div>
+    )
+}
