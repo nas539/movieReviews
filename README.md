17e42eda6fcad392a6837ad896775997beb8137b
Nicholas Sharp <naas8905@gmail.com>
2020-08-08 12:51:58 -0700

    Feature: basic login setup

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 > This is a movie review page made for a coding assessment for an apprenticeship for scalar.io
 
-*Fork from [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
---
+++ src/components/pages/login.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import Cookies from 'js-cookie';
+
+export default class Login extends Component {
+    constructor(props){
+        super(props);
+
+        if (Cookies.get("username")) {
+            window.location.href=("/#/movie/edit")
+          }
+
+        this.state = {
+            username: "",
+            password: "",
+            errorMessage: " Go ahead, login!"
+        }
+
+        this.handleLoginChange = this.handleLoginChange.bind(this);
+        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    }
+
+    handleLoginChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    handleLoginSubmit(event) {
+        event.preventDefault();
+        Cookies.get("username")
+        this.setState({
+            errorMessage: "logging in.."
+        })
+        if (this.state.username === "" || this.state.password === "") {
+            this.setState({ errorMessage: "All feilds required!" })
+        }
+        else {
+        }
+    }
+
+    render() {
+        return (
+            <div className="login-body-wrapper">
+                <div className="login-sections-wrapper">
+                    <div className="login-section">
+                        <p>Username: </p>
+                        <input 
+                            type="text" 
+                            name="username" 
+                            username={this.state.username}
+                            placeholder="Username"
+                            onChange={this.handleLoginChange}
+                        />
+                    </div>
+                    <div className="login-section"> 
+                        <p>Password: </p>
+                        <input 
+                            type="password" 
+                            name="password" 
+                            password={this.state.password} 
+                            placeholder="Password"
+                            onChange={this.handleLoginChange}
+                        />
+                    </div>
+                    <div className="login-section">
+                        <button type="submit" onClick={this.handleLoginSubmit}>Login</button>    
+                        <p id="error">{this.state.errorMessage}</p>
+                    </div>
+                </div>    
+            </div>
+        )
+    }
+}
